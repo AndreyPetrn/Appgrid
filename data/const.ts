da3d7234c87005464a9c7504b233395a75eb6821
Андрей <91607494+AndreyPetrn@users.noreply.github.com>
2024-02-01 14:38:06 +0200

    updated config data

--- data/const.ts
+++ data/const.ts
@@ -1,13 +1,13 @@
 export class Constants {
-    public static readonly APP_GRID_LOGIN: string = 'https://app.getappgrid.com/login';
-    public static readonly INTERNAL_ADMIN_LOGIN: string = 'https://app.getappgrid.com/ia/login';
-    public static readonly INTERNAL_ADMIN_DASHBOARD: string = 'https://app.getappgrid.com/ia/';
-    public static readonly INTERNAL_ADMIN_ACCOUNTS: string = 'https://app.getappgrid.com/ia/accounts';
-    public static readonly INTERNAL_ADMIN_USERS: string = 'https://app.getappgrid.com/ia/users';
-    public static readonly INTERNAL_ADMIN_PLANS: string = 'https://app.getappgrid.com/ia/plans';
-    public static readonly INTERNAL_ADMIN_THEMES: string = 'https://app.getappgrid.com/ia/themes';
-    public static readonly INTERNAL_ADMIN_SETTINGS: string = 'https://app.getappgrid.com/ia/settings';
-    public static readonly USER_ADMIN_SETTINGS: string = 'https://app.getappgrid.com/account-settings';
-    public static readonly USER_ADMIN_THEMES: string = 'https://app.getappgrid.com/themes';
-    public static readonly USER_APP_SETTINGS: string = 'https://app.getappgrid.com/settings/app';
+    public static readonly APP_GRID_LOGIN: string = 'login';
+    public static readonly INTERNAL_ADMIN_LOGIN: string = 'login';
+    public static readonly INTERNAL_ADMIN_DASHBOARD: string = 'ia';
+    public static readonly INTERNAL_ADMIN_ACCOUNTS: string = 'accounts';
+    public static readonly INTERNAL_ADMIN_USERS: string = 'users';
+    public static readonly INTERNAL_ADMIN_PLANS: string = 'plans';
+    public static readonly INTERNAL_ADMIN_THEMES: string = 'themes';
+    public static readonly INTERNAL_ADMIN_SETTINGS: string = 'settings';
+    public static readonly USER_ADMIN_SETTINGS: string = 'account-settings';
+    public static readonly USER_ADMIN_THEMES: string = 'themes';
+    public static readonly USER_APP_SETTINGS: string = 'app';
 }
